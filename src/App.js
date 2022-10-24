cafbdd5c98f9e0ab8421255b8fa9c1debfe99a3c
Wajdi Bennour <bennour.wajdi.pro@gmail.com>
2022-10-23 23:06:32 -0300

    Tailwind

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import {useState} from 'react';
 
 function App() {
   
-  const[role, setRole] = useState('dev');
+  const[role, setRole] = useState('Unity Developper');
   const show = true;
   
   return (
-    <div className="App bg-red-500">
+    <div className="App">
       {  show ?
         <>
           <input type="text" onChange=
@@ -18,11 +18,13 @@ function App() {
               setRole(e.target.value);
             }
           }/>
-          <Employee name= 'John' role='CEO'/>
+        <div class='flex flex-wrap'>
+          <Employee name= 'John' role='CEO' src='https://images.pexels.com/photos/2047905/pexels-photo-2047905.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'/>
           <br></br>
-          <Employee name= 'wajdi' role={role}/>
+          <Employee name= 'Wajdi' role={role} class='decoration-red-300'/>
           <br></br>
           <Employee name = 'Kevin'/>
+        </div>
         </>
         :
         <p>You cannot see this</p>
